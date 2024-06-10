43b0904237ff4f16bec11b7f7ec25e48043f291f
ychic1109-DA <ychic1109@gmail.com>
2024-06-10 18:12:55 +0900

    첫 작업

---
+++ Mukjibba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mukjibba
+//
+//  Created by t2023-m0013 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
